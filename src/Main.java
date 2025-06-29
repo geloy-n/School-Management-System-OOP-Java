fc83358c6881e7ecac79cc534cec5314492f8863
Angelo <angelonieves0105@gmail.com>
2025-06-29 14:29:42 +0800

    Adding teacher output

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import models.Student;
+import models.Teacher;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,12 +7,16 @@ public class Main {
         // Create Students
         Student s1 = new Student("John", 20);
         Student s2 = new Student("Mark", 21);
+        Teacher t1 = new Teacher("Matthew", 30);
 
         // Display info
         s1.displayInfo();
+        s2.displayInfo();
+        t1.displayInfo();
 
         // Counter
         System.out.println("Total Number of Students: " + Student.getTotalStudents());
+        System.out.println("Total Number of Teachers: " + Teacher.getTotalTeachers());
 
     }
 }
