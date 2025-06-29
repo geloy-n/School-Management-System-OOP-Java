d679cb0fac040d7b3419fd1649d0f4fac384f13e
Angelo <angelonieves0105@gmail.com>
2025-06-29 14:30:38 +0800

    Inital commit for Teacher class

--- src/models/Teacher.java
+++ src/models/Teacher.java
@@ -1,4 +1,28 @@
 package models;
 
-public class Teacher {
+public class Teacher extends Person {
+
+
+    private final int teacherId;
+    private static int totalTeachers = 0;
+
+    // Constructor
+    public Teacher(String name, int age){
+        super(name, age);
+        totalTeachers++;
+        this.teacherId = totalTeachers;
+    }
+
+    // Method overriding
+    @Override
+    public void displayInfo(){
+        System.out.println("Teacher ID: " + teacherId + ", Name: " + name + ", Age: " + age);
+    }
+
+    // Getters
+
+    public static int getTotalTeachers() {
+        return totalTeachers;
+    }
+
 }
