373da350b4e00ff27127b536e631e7a04a4e4b8c
Angelo <angelonieves0105@gmail.com>
2025-06-29 15:07:06 +0800

    Added baseSlary and computeSalary from Payable Interface

--- src/models/Teacher.java
+++ src/models/Teacher.java
@@ -1,25 +1,41 @@
 package models;
 
-public class Teacher extends Person {
+public class Teacher extends Person implements Payable{
 
-
+    private final double baseSalary;
     private final int teacherId;
     private static int totalTeachers = 0;
 
     // Constructor
-    public Teacher(String name, int age){
+    public Teacher(String name, int age, double baseSalary){
         super(name, age);
+        this.baseSalary = baseSalary;
         totalTeachers++;
         this.teacherId = totalTeachers;
     }
 
+
+
+    // Implementing computeSalary()
+    @Override
+    public double computeSalary(){
+        return baseSalary;
+    }
+
     // Method overriding
     @Override
-    public void displayInfo(){
-        System.out.println("Teacher ID: " + teacherId + ", Name: " + name + ", Age: " + age);
+    public void displayInfo() {
+        System.out.println("Teacher ID: " + teacherId + ", Name: " + name +
+                ", Age: " + age +
+                ", Base Salary: " + baseSalary +
+                ", Total Salary: " + computeSalary());
     }
 
     // Getters
+
+    public double getSalary(){
+        return baseSalary;
+    }
 
     public static int getTotalTeachers() {
         return totalTeachers;
