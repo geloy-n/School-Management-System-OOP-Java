dc173b1c80ee46c3ab808e75db033ee66a713593
Angelo <angelonieves0105@gmail.com>
2025-06-30 19:45:57 +0800

    Adding comments on Course class

--- src/models/Course.java
+++ src/models/Course.java
@@ -4,12 +4,14 @@ public class Course {
     
     private final String courseName;
     private final String courseCode;
-    
+
+    // Constructor
     public Course(String courseName, String courseCode) {
         this.courseName = courseName;
         this.courseCode = courseCode;
     }
-    
+
+    // Getters
     public String getCourseName() {
         return courseName;
     }
