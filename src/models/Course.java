16a7030ab24e3e2de098a3b09f8d8ba29bd3412a
Angelo <angelonieves0105@gmail.com>
2025-06-30 19:42:06 +0800

    Initial commit for course class

--- src/models/Course.java
+++ src/models/Course.java
@@ -1,4 +1,21 @@
 package models;
 
 public class Course {
+    
+    private final String courseName;
+    private final String courseCode;
+    
+    public Course(String courseName, String courseCode) {
+        this.courseName = courseName;
+        this.courseCode = courseCode;
+    }
+    
+    public String getCourseName() {
+        return courseName;
+    }
+    
+    public String getCourseCode() {
+        return courseCode;
+    }
+    
 }
