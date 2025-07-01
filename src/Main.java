75c497f2bac47af05d83adf08bdd1d51f87490f2
Angelo <angelonieves0105@gmail.com>
2025-07-01 20:09:37 +0800

    updated ouput for testing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import models.*;
 
+import java.util.ArrayList;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -11,9 +13,16 @@ public class Main {
         // Create Teacher/s
         Teacher t1 = new Teacher("Matthew", 30, 40000);
 
+        // Create Teacher/s
+        Admin a1 = new Admin("Luke", 50000, 8000);
+
         // Create Course
         Course math = new Course("Mathematics 101", "MATH101");
         Course science = new Course("Science 101", "SCI101");
+
+        // test adding course for teacher
+        t1.addCourse(math);
+        t1.addCourse(science);
 
         // Display info
         s1.displayInfo();
@@ -26,14 +35,6 @@ public class Main {
         System.out.println("Total Number of Teachers: " + Teacher.getTotalTeachers());
         System.out.println();
 
-        // computeSalary via Payable interface
-        System.out.println("Computed Salary: " + t1.computeSalary() + " for Prof. " + t1.getName());
-        System.out.println();
-
-        // test adding course for teacher
-        t1.addCourse(math);
-        t1.addCourse(science);
-
         // test adding course for students
         s1.enrollCourse(math);
         s2.enrollCourse(science);
@@ -42,6 +43,40 @@ public class Main {
         t1.displayCourses();
         s1.displayCourses();
         s2.displayCourses();
+
+        // test Payable arraylist
+        ArrayList<Payable> payables = new ArrayList<>();
+        payables.add(t1);
+        payables.add(a1);
+
+        // test display individual salaries compute total
+        // Polymorphism: storing different Payable objects (Teacher, Admin) in one list
+        System.out.println();
+        double totalPayroll = 0;
+
+        // Polymorphism: runtime polymorphism occurs here
+        // Each Payable object responds to computeSalary() based on its actual class
+        for (Payable p: payables) {
+            double salary = p.computeSalary();
+            totalPayroll += salary;
+
+            if (p instanceof Teacher t) {
+                System.out.println("Teacher: " + t.getName() + " | Base Salary: " + t.getSalary() + " | Computed Salary: " + salary);
+            } else if (p instanceof Admin a) {
+                System.out.println("Admin: " + a.getName() + " | Total Salary: " + salary);
+            }
+        }
+
+        // test Adjust salary
+        t1.setSalary(45000);
+
+        // test Remove course
+        t1.removeCourse(science);
+
+        // Display updated info
+        System.out.println();
+        t1.displayInfo();
+        t1.displayCourses();
 
     }
 }
