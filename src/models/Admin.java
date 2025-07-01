6d671a5057efa33a79ecf3a7ec1c7beb6fee5c14
Angelo <angelonieves0105@gmail.com>
2025-07-01 20:10:17 +0800

    Initial commit of Admin Class

--- src/models/Admin.java
+++ src/models/Admin.java
@@ -1,4 +1,32 @@
 package models;
 
-public class Admin {
+public class Admin implements Payable {
+    private final String name;
+    private double baseSalary;
+    private final double allowance;
+
+
+    // Constructor
+    public Admin(String name, double baseSalary, double allowance) {
+        this.name = name;
+        this.baseSalary = baseSalary;
+        this.allowance = allowance;
+    }
+
+    @Override
+    public double computeSalary() {
+        return baseSalary + allowance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSalary(double newSalary) {
+        if (newSalary >= 0) {
+            System.out.println("Updating salary for " + name + " from " + baseSalary + " to " + newSalary);
+            baseSalary = newSalary;
+        }
+    }
+
 }
